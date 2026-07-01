9f5e8ff0b498d3ccc0c51abcf0c8749d8aa60d02
Akezhun <akejanduiseen@gmail.com>
2026-07-01 19:37:06 +0500

    Delete core directory

--- core/history.py
+++
@@ -1,9 +0,0 @@
-from core.utils import now
-
-
-def log(task, action, details=None):
-    task.setdefault("history", []).append({
-        "action": action,
-        "time": now().isoformat(),
-        "details": details or {},
-    })
